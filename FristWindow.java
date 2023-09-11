35606faee2d254d90b0d2e4dd576808a69cd93ba
Hsutra <96309579+Hsutra@users.noreply.github.com>
2023-09-11 14:39:31 +0300

    Update FristWindow.java
    Добавлено 6 кнопок - 4.2

--- FristWindow.java
+++ FristWindow.java
@@ -8,9 +8,23 @@ public class Graphics {
         JButton b1 = new JButton("Кнопка 1");
         JButton b2 = new JButton("Кнопка 2");
         JButton b3 = new JButton("Кнопка 3");
+        JButton b4 = new JButton("Кнопка 4");
+        JButton b5 = new JButton("Кнопка 5");
+        JButton b6 = new JButton("Кнопка 6");
+        JButton b7 = new JButton("Кнопка 7");
+        JButton b8 = new JButton("Кнопка 8");
+        JButton b9 = new JButton("Кнопка 9");
+
         p.add(b1);
         p.add(b2);
         p.add(b3);
+        p.add(b4);
+        p.add(b5);
+        p.add(b6);
+        p.add(b7);
+        p.add(b8);
+        p.add(b9);
+
         f.setSize(500,500);
         f.add(p);
 
